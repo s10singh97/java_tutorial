3434a7b69d190ff0f47d062b41aa9948d361cdf1
s10singh97 <s10singh97@yahoo.in>
2018-06-08 18:07:38 +0530

    foreach

---
+++ foreach.java
@@ -0,0 +1,8 @@
+class ForEach{
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 85, 56};
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
