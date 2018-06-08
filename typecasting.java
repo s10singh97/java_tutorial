b1778e2287b69f366ab6c7ba23c21178ce1db2ba
s10singh97 <s10singh97@yahoo.in>
2018-06-08 17:34:37 +0530

    typecasting

---
+++ typecasting.java
@@ -0,0 +1,8 @@
+class TypeCast{
+    public static void main(String[] args) {
+        float a = 10.5f;
+        int b = (int)(a);
+        System.out.println(a);
+        System.out.println(b);
+    }   
+}
