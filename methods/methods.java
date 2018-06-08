ab3cce331f90dfecb5eda483c4db6593b4811929
s10singh97 <s10singh97@yahoo.in>
2018-06-08 18:15:57 +0530

    methods

---
+++ methods/methods.java
@@ -0,0 +1,11 @@
+/** The Calculator class provides methods to get addition and subtraction of given 2 numbers.*/
+public class methods{
+    /** The add() method returns addition of given numbers.*/
+    public static int add(int a, int b) {
+        return (a+b);
+    }
+    /** The sub() method returns subtraction of given numbers.*/
+    public static int subtract(int a, int b) {
+        return (a-b);
+    }
+}
